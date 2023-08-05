89552e6c848eda7a0d705820e638c575b8c76653
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-08-05 22:44:12 +1000

    made the project bug free to add the redux functions to it> successfull and now onto redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import store from "./store.js";
-import { Provider } from "react-redux";
+// import store from "./store.js";
+// import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
