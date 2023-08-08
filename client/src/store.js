25afcaed10e6f4f193febd2d22ae894e19fab41f
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-08-08 23:31:05 +1000

    added the user reducer to the store

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,12 @@
 // import React from "react";
 import { configureStore } from "@reduxjs/toolkit";
 import transactionReducer from "./pages/transaction/transactionSlice.js";
+import userReducer from "./pages/user/userSlice.js";
 
 const store = configureStore({
   reducer: {
     transactions: transactionReducer,
+    user: userReducer,
   },
 });
 
