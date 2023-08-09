b7e4e3f26d9dbb01e8771e017c00b2b9503c1a74
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-08-09 23:17:25 +1000

    added the postDataAction to post the transaction daata to the table

--- client/src/pages/transaction/transactionAction.js
+++ client/src/pages/transaction/transactionAction.js
@@ -1,7 +1,19 @@
-import { getTransaction } from "../../helpers/axiosHelper";
+import { getTransaction, postNewTransaction } from "../../helpers/axiosHelper";
 import { setTransactions } from "./transactionSlice";
 
 export const fetchDataAction = () => async (dispatch) => {
   const { status, message, trans } = await getTransaction();
   status === "success" && trans.length && dispatch(setTransactions(trans));
+};
+
+const postDataAction = () => async (dispatch) => {
+  const user = JSON.parse(sessionStorage.getItem("user"));
+
+  const userId = user._id;
+  const { status, message } = await postNewTransaction({
+    ...transaction,
+    userId,
+  });
+
+  // status === "success" && fetchData();
 };
