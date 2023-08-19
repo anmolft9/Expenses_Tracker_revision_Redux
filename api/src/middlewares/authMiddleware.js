76b4f6fd695c7512f8caf0f36ffff3dfa977b111
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-08-19 21:54:55 +1000

    adding the middleWare to make the authentication more stronger>

---
+++ api/src/middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+import { getOneUser } from "../models/userModel/userModel.js";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    //do authorisation header Exist?
+
+    const { authorization } = req.headers;
+
+    if (authorization) {
+      //do user exist in the DB?
+      const user = await getOneUser({ _id: authorization });
+
+      if (user?._id) {
+        req.userInfo = user;
+        return next();
+      }
+    }
+
+    res.status(403).json({
+      status: "error",
+      message: "Forbidden /not Allowed",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
