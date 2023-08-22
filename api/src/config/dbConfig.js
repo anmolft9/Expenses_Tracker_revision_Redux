a0cd7a2cb8e0d2c87afe4b6eb92f162ffe72ffe0
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-08-22 23:27:26 +1000

    added the .env file to store the database connection link and also installed dotenv on the backend

--- api/src/config/dbConfig.js
+++ api/src/config/dbConfig.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   try {
-    const MONGO_CLIENT = "mongodb://localhost/expense_tracker_revision_back";
-    const conn = mongoose.connect(MONGO_CLIENT);
+    const conn = mongoose.connect(process.env.MONGO_CLIENT);
     if (conn) {
       console.log("DB connected");
     }
