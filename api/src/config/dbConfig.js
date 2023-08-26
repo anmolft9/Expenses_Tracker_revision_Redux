e3b3de8a02c885ce769c321d44ad2653ada84899
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-08-26 21:56:14 +1000

    added some comments > completed the project full and final> onto CMS setup>

--- api/src/config/dbConfig.js
+++ api/src/config/dbConfig.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   try {
+    //mongoclient to the .env file for security purposes
+
     const conn = mongoose.connect(process.env.MONGO_CLIENT);
     if (conn) {
       console.log("DB connected");
